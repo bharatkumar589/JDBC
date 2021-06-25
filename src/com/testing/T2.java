91cf126f1800a66d8070a4740c0a790a668318f9
bharat <kbharat@pkgblobal.com>
2021-06-25 09:19:14 -0700

    test

--- src/com/testing/T2.java
+++
@@ -1,9 +0,0 @@
-package com.testing;
-
-public class T2 {
-
-	public static void main(String[] args) {
-		// Test the T2
-		// hello
-	}
-}
--- src/com/testing/T4.java
+++ src/com/testing/T4.java
@@ -3,8 +3,6 @@ package com.testing;
 public class T4 {
 	
 	public static void main(String[] args) {
-		// test T4
-		//sdfsd
 	}
 
 }
